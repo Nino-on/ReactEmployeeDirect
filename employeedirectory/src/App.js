6c8ae395de5eb79f51749568db4e045b1a1ee3ff
Ren <raulnino24@gmail.com>
2020-05-13 00:44:34 -0400

    searchfield

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 import './App.css';
 import Table from  './components/employee_list.js';
+import Searchbar from  './components/search.js';
 import employee from './Employee.json';
+
+
+
 
 class App extends Component{
   state={
     employee:employee
   };
 
+
  
   render() {
     console.log (employee)
       return(
+        
         <div>
+          <Searchbar
+           
+           
+          />
           {this.state.employee.map((employee,index)=>(
             <Table key={index}
               id={employee.id}
@@ -20,6 +30,7 @@ class App extends Component{
               occupation={employee.occupation}
               location={employee.location}
               />
+              
           ))};
         </div>
       );

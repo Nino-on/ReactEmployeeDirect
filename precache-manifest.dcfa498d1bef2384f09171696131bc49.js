d4b627023bd8bc4aa66307b377bfac1b49c5ad4c
Ren <raulnino24@gmail.com>
2020-05-13 23:14:08 -0400

    Updates

---
+++ precache-manifest.dcfa498d1bef2384f09171696131bc49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99823e9ecd1cd8450f7b414ef697b550",
+    "url": "/ReactEmployeeDirect/index.html"
+  },
+  {
+    "revision": "a596e5dfebc5624787e2",
+    "url": "/ReactEmployeeDirect/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "945e907afdcc005587ef",
+    "url": "/ReactEmployeeDirect/static/js/2.a03a31ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactEmployeeDirect/static/js/2.a03a31ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a596e5dfebc5624787e2",
+    "url": "/ReactEmployeeDirect/static/js/main.a71cf504.chunk.js"
+  },
+  {
+    "revision": "2dd83b3ba9c7a25815bf",
+    "url": "/ReactEmployeeDirect/static/js/runtime-main.5fe4d96f.js"
+  }
+]);
